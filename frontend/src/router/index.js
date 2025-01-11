ab2b9e7c137e0c1017474a39fd7d8cb588fe8971
daydream <admin@gitea.com>
2025-01-11 10:04:03 +0800

    basic template

---
+++ frontend/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/userInfo',
+      name: 'userInfo',
+      component: () => import('../views/UserInfoView.vue')
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: () => import('../views/SettingView.vue')
+    },
+    {
+      path: '/book/details/:id',
+      name: 'bookDetail',
+      component: () => import("../views/BookDetailView.vue")
+    }
+  ],
+})
+
+export default router
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>关于</h1>
+    <a href="https://github.com/DengNianxin/BookManageMentSystem">Github 项目地址</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
